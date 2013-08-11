e675dbcebdf9fe02af87ad9ae05c6fa92908b61f
jaredtritz <jaredtritz@gmail.com>
2013-08-11 11:41:08 -0400

    new version of authors checkout stabalizes diciontary checkout

--- authors_checkout.sh
+++ authors_checkout.sh
@@ -2,10 +2,10 @@
 
 #export SCRIPT_DIR=$(pwd)
 
-echo 'begin script'
+echo 'begin script' 
 cd ~jtritz/bitbucket/ecoach_webapps/mydata6/mts6
 svn update
-svn up -r 1424 mts.dictionary
+svn update -r1424 mts.dictionary
 source ~jtritz/virtualenv/v1/bin/activate
 python ~jtritz/bitbucket/ecoach_webapps/manage.py collectstatic --noinput --settings=mydata6.settings
 echo 'end of script'
