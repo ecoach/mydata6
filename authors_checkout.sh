cd4eeb21e9d84ab992a9f74540797ec9f370b0e1
jaredtritz <jtritz@umich.edu>
2013-08-11 11:07:06 -0400

    stable dictionary for mcdb

--- authors_checkout.sh
+++ authors_checkout.sh
@@ -5,6 +5,7 @@
 echo 'begin script'
 cd ~jtritz/bitbucket/ecoach_webapps/mydata6/mts6
 svn update
+svn up -r 1424 mts.dictionary
 source ~jtritz/virtualenv/v1/bin/activate
 python ~jtritz/bitbucket/ecoach_webapps/manage.py collectstatic --noinput --settings=mydata6.settings
 echo 'end of script'
