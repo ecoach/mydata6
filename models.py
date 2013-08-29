1369655e85f002dd2bc263f03298fe19a9b7b885
jaredtritz <jtritz@umich.edu>
2013-08-29 19:42:13 -0400

    cleanup the model to match dictionary, removing fields so no db mod required

--- models.py
+++ models.py
@@ -467,10 +467,6 @@ class Source1(SubjectData):
     Pred_MostProb_PostExam1 = models.IntegerField(null=True, blank=True)
     Pred_MostProb_PostExam2 = models.IntegerField(null=True, blank=True)
     Pred_MostProb_PostExam3 = models.IntegerField(null=True, blank=True)
-    Pred_Dist_Intial = models.CharField(max_length=20, null=True, blank=True)
-    Pred_Dist_PostExam1 = models.CharField(max_length=20, null=True, blank=True)
-    Pred_Dist_PostExam2 = models.CharField(max_length=20, null=True, blank=True)
-    Pred_Dist_PostExam3 = models.CharField(max_length=20, null=True, blank=True)
 
 class EmptySource(SubjectData):
     pass
